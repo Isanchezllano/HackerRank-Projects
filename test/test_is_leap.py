0514168bcc494c850405f6968462e6f7d005ecbd
Isabella <isanchezllano@gmail.com>
2025-02-20 16:00:50 -0500

    Mi primer commit

---
+++ test/test_is_leap.py
@@ -0,0 +1,14 @@
+from leap_year import is_leap
+
+
+def test_is_leap():
+    year = 1900
+    result = is_leap(year)
+
+    assert result == True
+
+def test_is_leap2():
+    year = 2024
+    result = is_leap(year)
+
+    assert result == True
